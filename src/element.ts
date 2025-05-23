2a9ee40b38df915c3ebe9c32c88707da3d01208a
ginger <26461046+gingerchew@users.noreply.github.com>
2025-05-23 10:24:43 -0400

    Adds commands support for removing items
    adds private getters for reading fixed elements from
    the shadow root more easily
    
    adds initial implementation for a "renderFileList" function
    
    adds initial command for removing items from file list

--- src/element.ts
+++ src/element.ts
@@ -1,11 +1,22 @@
-import { lerp, insertHTML, on, off } from "./utils";
+import { lerp, insertHTML, on, off, hash } from "./utils";
 // @ts-ignore
 import FileLakeStyles from "./styles.css?raw"; // Update to import attributes once supported by Vite;
+
+interface CommandEvent {
+	source: HTMLButtonElement;
+	command: string;
+}
 
 export class FileLake extends HTMLElement {
 	static formAssociated = true;
 	// #internals = this.attachInternals();
 	#root = this.attachShadow({ mode: "open" });
+	get #fileInput() {
+		return this.#root.querySelector<HTMLInputElement>('[type="file"]')!;
+	}
+	get #files() {
+		return this.#fileInput!.files;
+	}
 	tpl() {
 		return `<div class="wrapper">
       <input type="file" id="${this.id}-upload" />
@@ -28,18 +39,63 @@ export class FileLake extends HTMLElement {
 		prevX: 0,
 		prevY: 0,
 	};
+	handleCommand(e: CommandEvent) {
+		const { command, source } = e;
+
+		switch (command) {
+			case "--remove":
+				const item = source.closest<HTMLLIElement>("[data-hash]");
+				if (!item || !this.#files) return;
+				const dt = new DataTransfer();
+				for (let i = 0; i < this.#files.length; i += 1) {
+					if (hash(this.#files[i].name) === item.dataset.hash)
+						continue;
+					dt.items.add(this.#files[i]);
+				}
+
+				this.#fileInput.files = dt.files;
+				this.renderFileList();
+		}
+	}
+	renderFileList() {
+		const fileList =
+			this.#root.querySelector<HTMLUListElement>(".file-list")!;
+		const { files } =
+			this.#root.querySelector<HTMLInputElement>('[type="file"]')!;
+
+		const tpl = (file: File) => `<li data-hash="${hash(file.name)}">
+            ${file.name}<button type="button" command="--remove" commandfor="${this.id}">&times;</button>
+        </li>`;
+		const l = files!.length;
+		if (!files || l === 0) return;
+
+		document.startViewTransition(() => {
+			fileList.innerHTML = "";
+			for (let i = 0; i < l; i += 1) {
+				const currentFile = files.item(i);
+				if (!currentFile) continue;
+				insertHTML(fileList, tpl(currentFile));
+			}
+		});
+	}
 	handleEvent(e: Event) {
-		if (e.type === "mousemove") {
+		if (e.type === "command") {
+			this.handleCommand(e as unknown as CommandEvent);
+		} else if (e.type === "mousemove") {
 			const evt = e as MouseEvent;
 			this.pos.y = evt.clientY;
 			this.pos.x = evt.clientX;
 		} else if (e.type === "change") {
 			const fileList = (e.target! as HTMLInputElement).files!;
-
-			insertHTML(
-				this.#root.querySelector(".file-list")!,
-				`<li>${fileList[fileList.length - 1].name}</li>`
-			);
+			document.startViewTransition(() => {
+				insertHTML(
+					this.#root.querySelector(".file-list")!,
+					`<li data-name=">
+                        ${fileList[fileList.length - 1].name}
+                        <button type="button" command="--remove-item" commandfor="${this.id}">&times;</button>
+                    </li>`
+				);
+			});
 		} else {
 			this.setMouseOver(e);
 		}
@@ -59,8 +115,10 @@ export class FileLake extends HTMLElement {
 	connectedCallback() {
 		const stylesheet = new CSSStyleSheet();
 		stylesheet.replaceSync(FileLakeStyles);
+		this.id = this.id || `${this.localName}-${Date.now()}`;
 		this.#root.adoptedStyleSheets.push(stylesheet);
 		this.#root.innerHTML = this.tpl();
+		on(this, "command", this, true);
 		on(this, "input", this, true);
 		on(this, "mousemove", this, true);
 		on(this, "mouseover", this, true);
@@ -69,6 +127,7 @@ export class FileLake extends HTMLElement {
 		requestAnimationFrame(f => this.render(f));
 	}
 	disconnectedCallback() {
+		off(this, "command", this, true);
 		off(this, "input", this, true);
 		off(this, "mousemove", this, true);
 		off(this, "mouseover", this, true);
