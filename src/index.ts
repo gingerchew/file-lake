8108f61b222d60f5e05ffcbb99efbae6529631f8
ginger <26461046+gingerchew@users.noreply.github.com>
2025-05-23 12:24:26 -0400

    Adds mouse-tracker to define function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { FileLake } from "./element";
 import { FLFileList } from "./file-list";
+import { FLMouseTracker } from "./mouse-tracker";
 
 export const define = (name = "file-lake") => {
+	customElements.define("mouse-tracker", FLMouseTracker);
 	customElements.define("file-list", FLFileList);
 	customElements.define(name, FileLake);
 };
