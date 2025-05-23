621910eb7ab6499764e6a2330ed30b59824bbc57
ginger <26461046+gingerchew@users.noreply.github.com>
2025-05-23 10:22:52 -0400

    Adds hash util for managing files more sanely

--- src/utils.ts
+++ src/utils.ts
@@ -7,13 +7,27 @@ export const insertHTML = (
 ) => el.insertAdjacentHTML(insert, html);
 export const on = (
 	el: HTMLElement,
-	event: keyof HTMLElementEventMap,
+	event: "command" | keyof HTMLElementEventMap,
 	cb: EventListenerOrEventListenerObject,
 	opt: boolean | AddEventListenerOptions
 ) => el.addEventListener(event, cb, opt);
 export const off = (
 	el: HTMLElement,
-	event: keyof HTMLElementEventMap,
+	event: "command" | keyof HTMLElementEventMap,
 	cb: EventListenerOrEventListenerObject,
 	opt: boolean | AddEventListenerOptions
 ) => el.removeEventListener(event, cb, opt);
+
+export const hash = (n = "" + Date.now()) => {
+	var hash = 0,
+		l = n.length,
+		i,
+		chr;
+	if (l === 0) return hash;
+	for (i = 0; i < l; i++) {
+		chr = n.charCodeAt(i);
+		hash = (hash << 5) - hash + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	return "" + hash;
+};
